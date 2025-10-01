4aec0413d4cdde5405e1b453e010f107ffb2d796
Mckenzie Scarborough <scarborough.mckenzie@gmail.com>
2025-10-01 17:08:49 -0400

    created rudimentary login page

---
+++ TrackPoint/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrackPoint.Models;
+
+namespace TrackPoint.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            // Placeholder for authentication logic
+            if (model.Username == "admin" && model.Password == "password")
+            {
+                // TODO: Set authentication cookie/session
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Invalid login attempt.");
+            return View(model);
+        }
+    }
+}
