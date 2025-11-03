3d5448bd73ad3a322060ae07361f1d80631c9260
SteveDaSteve <steven@addressplus.net>
2025-11-02 21:43:13 -0500

    Initial implementation of Location Registration Form

--- TrackPoint/Controllers/AssetController.cs
+++ TrackPoint/Controllers/AssetController.cs
@@ -12,5 +12,25 @@ namespace TrackPoint.Controllers
 		{
 			return View(Asset.SampleAssets);
 		}
+
+		/**
+		 *  Return the view for the Location Add Form
+		 */
+		public IActionResult LocationAdd()
+		{
+			return View();
+		}
+
+		/**
+		 *  Add the new location to the database and redirect to the index
+		 */
+		public IActionResult NewLocation(Location l)
+		{
+			// Add the new Location to database and redirect the user to the index
+
+			// Log the Location to the console for debugging purposes
+			Console.WriteLine($"New Locatoin Added: {l.Name}, {l.Abbreviation}");
+			return View("../Home/Index");
+		}
 	}
 }
