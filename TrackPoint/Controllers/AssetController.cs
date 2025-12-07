b8df89a4148a8bc75eca1d0a3fc35f6cd00fc7c2
Steven Taylor <steven@addressplus.net>
2025-12-06 21:03:06 -0800

    Implement asset editing function

--- TrackPoint/Controllers/AssetController.cs
+++ TrackPoint/Controllers/AssetController.cs
@@ -38,11 +38,11 @@ namespace TrackPoint.Controllers
 		 *  Return the view for the Category Add Form
 		 */
 		public IActionResult CategoryAdd()
-    {
-      return View();
-    }
+        {
+          return View();
+        }
     
-    /*
+        /*
 		 *  Return the view for the Asset Add Form
 		 */
 		public IActionResult AssetAdd()
@@ -60,9 +60,9 @@ namespace TrackPoint.Controllers
 			// Log the category to the console for debugging purposes
 			Console.WriteLine($"New Category Added: {c.Name}, {c.Abbreviation}");
 			return View("../Home/Index");
-    }
+        }
      
-    /* 
+        /* 
 		 *  Add the new asset to the database and redirect to the Asset Browser
 		 */
 		public IActionResult NewAsset(Asset a)
@@ -77,13 +77,48 @@ namespace TrackPoint.Controllers
 			Console.WriteLine($"New Asset Added: {a.AssetTag}, {a.Make}, {a.Model}, {a.Category}, {a.Location}, {a.IssuedTo}, {a.Status}, {a.Notes}");
 			return View("AssetBrowser", Asset.SampleAssets);
 		}
-    
+
         /**
+         * Return the view for editing assets with the selected asset passed as the model
+         */
+        public IActionResult AssetEdit(string AssetTag)
+        {
+			var asset = Asset.SampleAssets.FirstOrDefault(a => a.AssetTag == AssetTag);
+			if (asset == null)
+			{
+				return NotFound();
+			}
+			return View(asset);
+        }
+
+		/**
+         * Return the view for editing assets with the selected asset passed as the model
+         */
+		public IActionResult UpdateAsset(Asset a)
+		{
+            // TODO: Replace with database functions and stop using SampleAssets
+
+            // Convert IEnumerable to List
+            List<Asset> assets = Asset.SampleAssets.ToList();
+            // Get the index of the asset that shares the tag of the one we are updating
+            int assetIndex = assets.IndexOf(assets.FirstOrDefault(asset => asset.AssetTag == a.AssetTag));
+            // Replace the asset with the updated one
+            assets[assetIndex] = a;
+            // Convert the asset List back to a IEnumerable and reassign it to Sample Assets
+            Asset.SampleAssets = assets;
+
+			// Log updated asset
+			Console.WriteLine($"Asset Updated: {a.AssetTag}, {a.Make}, {a.Model}, {a.Category}, {a.Location}, {a.IssuedTo}, {a.Status}, {a.Notes}");
+
+			return View("AssetBrowser", Asset.SampleAssets);
+		}
+
+		/**
          * Return the view for the Transfer Log with the sample data sorted by TransferDate descending as the 
          */
-        // TODO: This is not a real transfer log since it's just sorting the assets by transfer date, it does not give a detailed history.
-        // Create a transfer log model in the future to properly track asset transfers.
-        public IActionResult TransferLog()
+		// TODO: This is not a real transfer log since it's just sorting the assets by transfer date, it does not give a detailed history.
+		// Create a transfer log model in the future to properly track asset transfers.
+		public IActionResult TransferLog()
         {
             var sorted = Asset.SampleAssets.OrderByDescending(a => a.TransferDate).ToList();
             return View(sorted);
