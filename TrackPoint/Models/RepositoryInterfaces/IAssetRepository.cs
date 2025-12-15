3e661534c954c22052db03c27130fa3827f56a33
Steven Taylor <steven@addressplus.net>
2025-12-15 13:10:33 -0800

    Created interfaces

---
+++ TrackPoint/Models/RepositoryInterfaces/IAssetRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace TrackPoint.Models.RepositoryInterfaces
+{
+    public interface IAssetRepository
+    {
+        IEnumerable<AssetModel> GetAssets();
+		AssetModel GetAssetByID(string assetId);
+        void AddAsset(AssetModel asset);
+        void RemoveAsset(AssetModel asset);
+		void RemoveAssetByID(string assetId);
+		void UpdateAsset(AssetModel asset);
+        void Save();
+    }
+}
---
+++ TrackPoint/Models/RepositoryInterfaces/ILocationRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace TrackPoint.Models.RepositoryInterfaces
+{
+    public interface ILocationRepository
+    {
+		IEnumerable<LocationModel> GetLocations();
+		LocationModel GetLocationByID(string locationId);
+		void AddLocation(LocationModel location);
+		void RemoveLocation(LocationModel location);
+		void RemoveLocationByID(string locationId);
+		void UpdateLocation(LocationModel location);
+		void Save();
+	}
+}
