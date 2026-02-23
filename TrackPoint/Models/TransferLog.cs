f80b83fd84e242aabca9715de6c5d00b29b3567d
mattFer2 <n01490036@unf.edu>
2026-02-23 02:15:22 -0500

    Created the Model and Service for the Transfer Log, along with a new set of Enums related to it.

---
+++ TrackPoint/Models/TransferLog.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using static TrackPoint.Models.Enums;
+
+namespace TrackPoint.Models
+{
+    public class TransferLog
+    {
+        [Key]
+        public int TransferLogId { get; set; }
+
+        // Foreign key to Asset
+        [Required]
+        public int AssetId { get; set; }
+        public Asset Asset { get; set; }
+
+        // Foreign key to IdentityUser for issuedTo
+        [Required]
+        public string BorrowerId { get; set; }
+        public IdentityUser Borrower { get; set; }
+
+        
+        // Foreign key for Asset Status
+        public string NewStatus { get; set; }
+        public Asset AssetStatus { get; set; }
+        public string OldStatus { get; set; } // Status before the change, if applicable
+    
+        [Required]
+        public eventType eventType { get; set; } // TODO: Confirm that this works with Enums.cs
+        public DateTime TransferDate { get; set; }
+}
+}
