80aeb520e764ee6ae3dbf3b5e444644f8951edf5
mattFer2 <n01490036@unf.edu>
2026-03-07 16:03:51 -0500

    Created the controller, view, and ViewModel for Approvals.

---
+++ TrackPoint/Controllers/ApprovalsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrackPoint.Data;
+using TrackPoint.Views.Approvals;
+
+namespace TrackPoint.Controllers
+{
+    public class ApprovalsController : Controller
+    {
+        ApplicationDbContext _context;
+        public ApprovalsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var approvalsModel = new ApprovalsTestViewModel();
+            {
+                approvalsModel._approvals = _context.Approvals.ToList();
+            }
+            return View(approvalsModel);
+        }
+    }
+}
---
+++ TrackPoint/Views/Approvals/ApprovalsTestViewModel.cs
@@ -0,0 +1,9 @@
+﻿using TrackPoint.Models;
+
+namespace TrackPoint.Views.Approvals
+{
+    public class ApprovalsTestViewModel
+    {
+        public IEnumerable<TrackPoint.Models.Approvals> _approvals; // TODO: Should this be IEnumerable or IQueryable?
+    }
+}
